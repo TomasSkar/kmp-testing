a5165b4247187481c7f6de6abe5ab7628c7705bf
TomasSkar <tomas.skarnulis@trafi.com>
2025-01-18 11:15:26 +0200

    use correct density

--- composeApp/src/iosMain/kotlin/com/test/kmp/todo/app/ui/rememberWindowSize.ios.kt
+++ composeApp/src/iosMain/kotlin/com/test/kmp/todo/app/ui/rememberWindowSize.ios.kt
@@ -2,21 +2,26 @@ package com.test.kmp.todo.app.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-actual fun getScreenWidth() = LocalWindowInfo
-    .current
-    .containerSize
-    .width
-    .dp
+actual fun getScreenWidth(): Dp {
+    val localWindowInfo = LocalWindowInfo.current
+    val localDensity = LocalDensity.current
+    val configuration = localWindowInfo.containerSize
+
+    return with(localDensity) { configuration.width.toDp() }
+}
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-actual fun getScreenHeight() = LocalWindowInfo
-    .current
-    .containerSize
-    .height
-    .dp
+actual fun getScreenHeight(): Dp {
+    val localWindowInfo = LocalWindowInfo.current
+    val localDensity = LocalDensity.current
+    val configuration = localWindowInfo.containerSize
+
+    return with(localDensity) { configuration.height.toDp() }
+}

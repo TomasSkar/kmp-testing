627f527a4bc95cce83d676241b41e61f5b5d92c2
TomasSkar <tomas.skarnulis@trafi.com>
2025-01-18 10:41:44 +0200

    fix window class size class

---
+++ composeApp/src/androidMain/kotlin/com/test/kmp/todo/app/ui/rememberWindowSize.android.kt
@@ -0,0 +1,17 @@
+package com.test.kmp.todo.app.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+
+@Composable
+actual fun getScreenWidth() = LocalConfiguration
+    .current
+    .screenWidthDp
+    .dp
+
+@Composable
+actual fun getScreenHeight() = LocalConfiguration
+    .current
+    .screenHeightDp
+    .dp
